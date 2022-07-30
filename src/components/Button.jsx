a50d3f49e3b3c24ab56024caadc2196e958543a1
Skullable <riyaanbinusman57@gmail.com>
2022-07-30 19:07:28 +0500

    changes to button and adding contact info

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,35 @@
 import * as React from 'react';
 import { makeStyles } from '@mui/styles';
 import Button from '@mui/material/Button';
+import { padding } from '@mui/system';
 
 const useStyles = makeStyles({
   root: {
     background: '#1d73be',
-    border: 0,
-    borderRadius: '50%',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: '#1d73be',
+    border: 'solid 3px #1d73be',
+    width: '250px',
+    height: '300px',
+    borderRadius: '50px',
+    color: 'White',
     height: 50,
     padding: '0 30px',
+    cursor: 'pointer',
+    margin: '15px',
+    fontFamily: 'Poppins',
+    fontWeight: 300,
+    transition: 'all .5s',
+    '&:hover':{
+      background: 'white',
+      color: '#1d73be',
+    }
   },
+  send:{ 
+    
+  }
 });
 
 export default function Hook() {
   const classes = useStyles();
-  return <Button className={classes.root}>Styled with Hook API</Button>;
+  const value = 'SEND MESSAGE'
+  return <input className={classes.root} type='submit' value={value}></input>;
 }
--- src/components/Contact-content.jsx
+++ src/components/Contact-content.jsx
@@ -1,9 +1,14 @@
+import React from "react";
 import { makeStyles } from "@mui/styles";
 import Button from "@mui/material/Button";
 import TextField from "@mui/joy/TextField";
 import Hook from "./Button";
+import * as L from "leaflet"
 import Box from '@mui/joy/Box'
-import { fontSize, lineHeight } from "@mui/system";
+import { color, fontSize, margin, padding } from "@mui/system";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
 //import { Map, TileLayer } from 'react-leaflet';
 //import { useState } from "react";
 //import osm from "./osm";
@@ -21,25 +26,52 @@ const useStyles = makeStyles({
     },
     contactfield:{
         display: "flex",
-        alignItems: 'center',
+        width: '90%',
+        justifyContent: 'space-between'
     },
     span:{
+        display: 'flex',
+        flexDirection: 'row',
         fontFamily: 'poppins',
         fontWeight: 500,
         color: '#202833',
         fontSize: '36px',
         lineHeight: '36px',
+        padding: '20px'
+    },
+    textcontainer:{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: '10px',
+    },
+
+    p:{
+        fontFamily: 'poppins',
+        fontSize: '16px',
+        fontWeight: 300,
+        lineHeight: '21px',
+        paddingLeft: '10px',
+        color: '#7f7f7f',
     },
     userinput:{
+        flexDirection: "column",
+        alignItems: 'left',
+        justifyContent: 'space-around',
+        flex:3,
+        width: '80%',
+        margin: '20px',
+    },
+    contactdata:{
+        flex: 3
+    },
+    contactinfo:{
         display: 'flex',
         flexDirection: "column",
         alignItems: 'left',
         justifyContent: 'space-around',
-        flex:2,
-        width: '80%'
-    },
-    contactinfo:{
-        flex: 1
+        width: '80%',
+        margin: '20px',
     },
     nameinput:{
         backgroundColor:'#efefef',
@@ -51,6 +83,7 @@ const useStyles = makeStyles({
 
         '&::placeholder':{
             fontFamily: 'Poppins',
+            fontWeight: 300,
             textTransform: 'uppercase',
         }
 
@@ -64,22 +97,27 @@ const useStyles = makeStyles({
         height: '300px',
         width: '600px',
 
+    },
+    icons:{
+        color: '#1d73be'
     }
 })
 
 
 export default function Contactcontent() {
     const classes = useStyles();
-//  let mapOptions ={
-    //        zoom: 13,
-    //        center: [31.50357, 74.31905],
-//    }
-//    let map = new Window.L.map('map', mapOptions);
-//    let layer = new Window.L.TileLayer('http://{s}.title.openstreetmap.org/{z}/{x}/{y}.png');
-//    map.addLayer(layer)
+/*--    var map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     maxZoom: 19,
+     attribution: '© OpenStreetMap'
+    }).addTo(map);
+    var marker = L.marker([51.5, -0.09]).addTo(map); --*/
 
     return (
         <div className={classes.contact}>
+           <div id="map">
+            <map></map>
+           </div>
            <div className={classes.contactfield}>
             <div className={classes.userinput}>
              <span className={classes.span}>Contact Form</span>
@@ -89,7 +127,14 @@ export default function Contactcontent() {
              <TextField className={classes.msginput} placeholder='YOUR MESSAGE' variant="solid"/>
              <Hook/>
             </div>
-            <div className={classes.contactinfo}></div>
+            <div className={classes.contactdata}>
+                <div className={classes.contactinfo}>
+                 <span className={classes.span}>Contact Info</span>
+                 <div className={classes.textcontainer}> <LocationOnIcon className={classes.icons} fontSize='12'/> <p className={classes.p}> FF 11 Center point plaza, Main Blvd, Block E 2 Gulberg III, Lahore, Punjab</p> </div>
+                 <div className={classes.textcontainer}> <CallIcon className={classes.icons} fontSize='12'/> <p className={classes.p}>	+92 311 1555082</p> </div>
+                 <div className={classes.textcontainer}> <EmailIcon className={classes.icons} fontSize='12'/> <p className={classes.p}> info@saispk.com</p> </div>
+                </div>
+            </div>
           </div> 
         </div>
     )
