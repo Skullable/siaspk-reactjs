4c3182996a45791baf479d05f3e2869ad78ece51
Skullable <riyaanbinusman57@gmail.com>
2022-07-31 20:40:58 +0500

    partially added slider

--- src/components/Home-content.jsx
+++ src/components/Home-content.jsx
@@ -1,32 +1,23 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
-import slider from "./Slider";
-import TextField from "@mui/joy/TextField";
-import Button from "./Button";
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import CallIcon from '@mui/icons-material/Call';
-import EmailIcon from '@mui/icons-material/Email';
-import { AccessTimeSharp } from "@mui/icons-material";
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import ReactDOM from "react-dom";
+import Slider from "./Slider";
 
 
 const useStyles = makeStyles({
-    Home:{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginBottom: '100px'
-    },
-})
-
+  Home: {
+    top: "100px",
+    alignItems: "center",
+    marginBottom: "100px",
+  },
+});
 
 export default function Homecontent() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.Home}>
-            <slider className={classes.slider}/>
-        </div>
-    )
+  return (
+    <div className={classes.Home}>
+      <Slider className={classes.slider} />
+    </div>
+  );
 }
-
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,45 +1,56 @@
-import React from 'react'
+import styled from "styled-components";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function slider() {
+const Carouselwrapper = styled.div`
+  width: 100%;
+  .carousel-control.left,
+  .carousel-control.right {
+    background: none;
+  }
+`;
+
+export default function Slider() {
   return (
-    <Carousel fade>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=First slide&bg=373940"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Second slide&bg=282c34"
-        alt="Second slide"
-      />
+    <Carouselwrapper>
+      <Carousel fade>
+        <Carousel.Item interval={1600} touch="true">
+          <img
+            className="d-block w-100"
+            src="Images/slider1.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1600}>
+          <img
+            className="d-block w-100"
+            src="Images/development.jpg"
+            alt="Second slide"
+          />
 
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Third slide&bg=20232a"
-        alt="Third slide"
-      />
-
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>
-          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-        </p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
-  )
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1600}>
+          <img
+            className="d-block w-100"
+            src="Images/technicalAdvisory.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Carouselwrapper>
+  );
 }
